2b79812d09f506e09be9f8d31cfe06cca76af21b
fasdf1 <whddls1747@gmail.com>
2022-08-26 13:47:25 +0900

    totalservice

---
+++ src/main/java/com/example/apitest/TotalService.java
@@ -0,0 +1,24 @@
+package com.example.apitest;
+
+import com.example.apitest.Answer.Answer;
+import com.example.apitest.Comment.Comment;
+import com.example.apitest.Question.Question;
+import com.example.apitest.Tag.Tag;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@Service
+public class TotalService {
+
+    Tag tag = new Tag(1L,"JAVA","자바");
+    public List<Object> findTotal(){
+        return List.of(
+                new Question(1L,"1번 글 제목","내용 1 번 입니다","작성자",tag, LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE)),
+                new Answer(1L,"1번 글 답글"),
+                new Comment(1L,"1번 글 댓글")
+        );
+    }
+}
